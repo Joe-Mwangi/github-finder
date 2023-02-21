375749c0b889dc04ad65904fe08c8dcde77bdf7b
Joe-Mwangi <joem99271@gmail.com>
2023-02-21 11:01:55 +0300

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
-    <div className="bg-purple-500">
-        <h1 className="text-xl">hello world</h1>
+    <div className="bg-purple-400">
+        <h1 className="text-2xl">hello world</h1>
     </div>
   )
 }
